778c71e952efa9e5416d413861d42f615e9fbee2
Stefanie <131598782+Stefmatron@users.noreply.github.com>
2024-02-24 11:12:46 +0100

    added the url for github

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Weather App
-        </p>
+        </h1>
         
       </header>
+      <footer>
+        This project was coded by Stefanie Neb and is {" "}
+        <a href="https://github.com/Stefmatron/reactwa-w5.git"
+          target="_blank"
+          >
+            open-sourced on Github.
+        </a>
+      </footer>
     </div>
   );
 }
